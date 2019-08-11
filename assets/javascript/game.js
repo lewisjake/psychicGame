9c9c00bec726a56cc27e0e5b9d77620f95af8740
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-08-10 23:18:48 -0700

    Confident in my code, however having issues with getting it to run.. not really sure why.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,22 +4,22 @@ var computerChoice = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
 // Set all the beginning values
 let wins = 0;
 let losses = 0;
-let guesses = 0;
-let guessesRemaining = 0;
+let guesses = 9;
+let guessesRemaining = 9;
 var guessesSoFar = [];
 var letterToGuess = null;
 
 // Have the computer generate a random letter from the array.
 var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 // Give the user 9 guesses
-function updateGuessesLeft() {
-    // Changes the html to guessesLeft
+function updateGuessesRemaining() {
+    // Changes the html to guessesRemaining
     document.querySelector('#guessesRemaining').innerHTML = "Guesses Left: " + guessesRemaining;
 };
 
 // Update the letterToGuess
 function updateletterToGuess() {
-    this.letterToGuess = this.computerChoice(Math.floor(Math.random() * this.computerChoice.length);
+    this.letterToGuess = this.computerChoice(Math.floor(Math.random() * this.computerChoice.length));
 };
 
 // Update the letters that the user has guessed already. Take what the user has input so far and display
@@ -41,15 +41,42 @@ var Reset = function () {
 
     updateletterToGuess();
     updateGuessesLeft();
-}
+
 
 // Set key release so it becomes users guess
-document.onkeyup =function(event) {
+document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    var check =computerChoice.includes(userGuess);
+    var check = computerChoice.includes(userGuess);
 
     if (check === false) {
         alert("That was an invalde guess, try again!");
         return false;
     }
+    else if (check === true) {
+        // If the user's choice was a char, it will add the guess to the array of guessed letters
+        guessesRemaining --; 
+        guessedSoFar.push(usersGuess);
+        updateGuessRemaining();
+        updateGuessesSoFar();
+
+        if (guessesRemaining > 0) {
+            if (userGuess == letterToGuess) {
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                userGuess = userGuess.toUpperCase();
+                Reset();
+            }
+        
+        } else if (guessesRemaining == 0) {
+            // User will lose and then we update the html to display a loss.
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            // App will reset after this.
+            Reset();
+        }
+    return false;
+} else {
+    alert("Error");
+}
+};
 }
